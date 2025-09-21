5c5e80a8aa750e674a4bedf7f3c46410cf3a8e27
LMD Network <118551627+lumemeduck@users.noreply.github.com>
2025-09-21 23:45:33 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,23 @@
-  self.addEventListener('install', event => {
-  self.skipWaiting();
+const CACHE_NAME = "rise-shine-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+// Install event
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', event => {
+// Fetch event
+self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
